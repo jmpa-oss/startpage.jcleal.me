baf2357dace309bd920b0b663942aa59c736f5dc
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2020-12-28 14:38:11 +1100

    clean up update-repository script, since it is having issues

--- bin/update-repository.sh
+++ bin/update-repository.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-# a basic script to clean up the logs in GitHub Actions for updating a repository using this template.
-#
-# NOTE: this should not be run locally.
+# a basic script to clean up the logs in GitHub Actions.
+# used in the template update mechanism, and should not be run locally.
+
+# funcs
+die() { echo "$1" >&2; exit "${2:-1}"; }
+
+# check project root
+[[ ! -d .git ]] \
+  && die "must be run from repository root directory"
 
 echo "##[group]Configuring GitHub user"
 git config user.name 'GitHub Actions'
@@ -9,7 +15,8 @@ git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
 echo "##[endgroup]"
 
 echo "##[group]Checking for any files that have changed"
-./bin/update-template.sh
+./bin/update-template.sh \
+  || die "failed to update template"
 echo "##[endgroup]"
 
 echo "##[group]Pushing"
